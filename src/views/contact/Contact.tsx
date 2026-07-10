670fd388e81ba392311895d7a7458c257943eb23
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-10 16:52:07 -0400

    Definido contenido de vista Contact

--- src/views/contact/Contact.tsx
+++ src/views/contact/Contact.tsx
@@ -1,9 +1,18 @@
 import './Contact.css'
 
 export default function() {
+  const links = [
+    {type: 'cristiancajiaos@outlook.com', link: 'mailto:cristiancajiaos@outlook.com'},
+    {type: 'Linkedin', link: 'https://www.linkedin.com/in/cristiancajiaos/'},
+    {type: 'Github', link: 'https://github.com/cristiancajiaos'}
+  ]
   return (
     <section className="contact">
-      <h1>Contact</h1>
+      {links.map((link) => 
+        <a href={link.link}>
+          {link.type}
+        </a>
+      )}
     </section>
   );
 }
